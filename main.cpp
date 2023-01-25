83764337347e4fb4359effd51313dffc389882af
Honarl <rogersmq@sunypoly.edu>
2023-01-25 15:20:19 -0500

    Brute force overloads

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "print.h"
 #include <iostream>
 
+using namespace printer;
 
-
-int main()
+int main(void)
 {
-    printer::printerclass::hi();
+    int testarray[5] {15, 5, 32, 1000, 500000000};
+
+
+    printerclass::hi(testarray, sizeof(testarray));
 
     return 0;
 }
--- print.h
+++ print.h
@@ -1,15 +1,69 @@
 #ifndef PRINT_H_INCLUDED
 #define PRINT_H_INCLUDED
 #include <iostream>
+using namespace std;
 
 namespace printer
 {
     class printerclass
     {
         public:
-            static void hi()
+            //Overloads for each data type
+            //ints
+            static void hi(int testarray[], int arraysize)
             {
-                std::cout<<"Yo.";
+                for (int i = 0; i < arraysize / sizeof(int); i++){
+                    cout<<testarray[i];
+                    cout<<" ";
+                }
+            }
+            //long long int error bait
+            static void hi(long testarray[], int arraysize)
+            {
+                for (int i = 0; i < arraysize / sizeof(long); i++){
+                    cout<<testarray[i];
+                    cout<<" ";
+                }
+            }
+            //floats
+            static void hi(float testarray[], int arraysize)
+            {
+                for (int i = 0; i < arraysize / sizeof(float); i++){
+                    cout<<testarray[i];
+                    cout<<" ";
+                }
+            }
+            //double
+            static void hi(double testarray[], int arraysize)
+            {
+                for (int i = 0; i < arraysize / sizeof(double); i++){
+                    cout<<testarray[i];
+                    cout<<" ";
+                }
+            }
+            //char
+            static void hi(char testarray[], int arraysize)
+            {
+                for (int i = 0; i < arraysize / sizeof(char); i++){
+                    cout<<testarray[i];
+                    cout<<" ";
+                }
+            }
+            //string
+            static void hi(string testarray[], int arraysize)
+            {
+                for (int i = 0; i < arraysize / sizeof(string); i++){
+                    cout<<testarray[i];
+                    cout<<" ";
+                }
+            }
+            //bool
+            static void hi(bool testarray[], int arraysize)
+            {
+                for (int i = 0; i < arraysize / sizeof(bool); i++){
+                    cout<<testarray[i];
+                    cout<<" ";
+                }
             }
     };
 }
