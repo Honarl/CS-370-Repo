ed5ce898f5a63f6a25d792393a14be608eb819b4
Betsy Kamas <kamase@sunypoly.edu>
2023-01-31 12:31:43 -0500

    foramtted printing and tests for all data types

--- print.h
+++ print.h
@@ -1,6 +1,7 @@
 #ifndef PRINT_H_INCLUDED
 #define PRINT_H_INCLUDED
 #include <iostream>
+#include <string>
 #include "rogueutil.h"
 
 using namespace std;
@@ -11,62 +12,262 @@ namespace printer
     class printerclass
     {
         public:
+			// countig number of digits in each number
+			static void countDigits(int testarray[], int numlength[], int length){
+				for(int i = 0; i < length; i++){
+					string s = to_string(testarray[i]);
+					numlength[i] = s.length();
+				}
+			}
+			// printing top and bottom of visual structure
+			static void printTopNBot(int testarray[], int arraysize){
+				int length = arraysize / sizeof(int);
+				int numlength[length] = {};
+				countDigits(testarray,numlength,length);
+				cout<<"+";
+				for(int i = 0; i < length; i++){
+					for(int y = 0; y < numlength[i]; y++){
+						cout<<"-";
+					}
+					cout<<"+";
+				}
+				cout<<endl;
+			}
+			// printing numbers with dividers between
+			static void printNums(int testarray[], int arraysize){
+				arraysize /= sizeof(int);
+				cout<<"|";
+				for(int i = 0; i < arraysize; i++){
+					cout<<testarray[i]<<"|";
+				}
+				cout<<endl;
+			}
+			// countig number of digits in each number
+			static void countDigits(long testarray[], int numlength[], int length){
+				for(int i = 0; i < length; i++){
+					string s = to_string(testarray[i]);
+					numlength[i] = s.length();
+				}
+			}
+			// printing top and bottom of visual structure
+			static void printTopNBot(long testarray[], int arraysize){
+				int length = arraysize / sizeof(long);
+				int numlength[length] = {};
+				countDigits(testarray,numlength,length);
+				cout<<"+";
+				for(int i = 0; i < length; i++){
+					for(int y = 0; y < numlength[i]; y++){
+						cout<<"-";
+					}
+					cout<<"+";
+				}
+				cout<<endl;
+			}
+			// printing numbers with dividers between
+			static void printNums(long testarray[], int arraysize){
+				arraysize /= sizeof(long);
+				cout<<"|";
+				for(int i = 0; i < arraysize; i++){
+					cout<<testarray[i]<<"|";
+				}
+				cout<<endl;
+			}
+			// countig number of digits in each number
+			static void countDigits(float testarray[], int numlength[], int length){
+				for(int i = 0; i < length; i++){
+					string s = to_string(testarray[i]);
+					numlength[i] = s.length();
+				}
+			}
+			// printing top and bottom of visual structure
+			static void printTopNBot(float testarray[], int arraysize){
+				int length = arraysize / sizeof(float);
+				int numlength[length] = {};
+				countDigits(testarray,numlength,length);
+				cout<<"+";
+				for(int i = 0; i < length; i++){
+					for(int y = 0; y < numlength[i]; y++){
+						cout<<"-";
+					}
+					cout<<"+";
+				}
+				cout<<endl;
+			}
+			// printing numbers with dividers between
+			static void printNums(float testarray[], int arraysize){
+				arraysize /= sizeof(float);
+				cout<<"|";
+				for(int i = 0; i < arraysize; i++){
+					cout<<testarray[i]<<"|";
+				}
+				cout<<endl;
+			}
+			// countig number of digits in each number
+			static void countDigits(double testarray[], int numlength[], int length){
+				for(int i = 0; i < length; i++){
+					string s = to_string(testarray[i]);
+					numlength[i] = s.length();
+				}
+			}
+			// printing top and bottom of visual structure
+			static void printTopNBot(double testarray[], int arraysize){
+				int length = arraysize / sizeof(double);
+				int numlength[length] = {};
+				countDigits(testarray,numlength,length);
+				cout<<"+";
+				for(int i = 0; i < length; i++){
+					for(int y = 0; y < numlength[i]; y++){
+						cout<<"-";
+					}
+					cout<<"+";
+				}
+				cout<<endl;
+			}
+			// printing numbers with dividers between
+			static void printNums(double testarray[], int arraysize){
+				arraysize /= sizeof(double);
+				cout<<"|";
+				for(int i = 0; i < arraysize; i++){
+					cout<<testarray[i]<<"|";
+				}
+				cout<<endl;
+			}			
+			// countig number of chars
+/*			static void countDigits(char testarray[], int numlength[], int length){
+				for(int i = 0; i < length; i++){
+					string s = to_string(testarray[i]);
+					numlength[i] = s.length();
+				}
+			}
+*/			// printing top and bottom of visual structure
+			static void printTopNBot(char testarray[], int arraysize){
+				int length = arraysize / sizeof(char);
+//				int numlength[length] = {};
+//				countDigits(testarray,numlength,length);
+				cout<<"+";
+				for(int i = 0; i < length; i++){
+					cout<<"-+";
+				}
+				cout<<endl;
+			}
+			// printing chars with dividers between
+			static void printNums(char testarray[], int arraysize){
+				arraysize /= sizeof(char);
+				cout<<"|";
+				for(int i = 0; i < arraysize; i++){
+					cout<<testarray[i]<<"|";
+				}
+				cout<<endl;
+			}
+			// countig number of digits in each number
+			static void countDigits(string testarray[], int numlength[], int length){
+				for(int i =0; i < length; i++){
+					numlength[i] = testarray[i].length();
+				}
+		
+			}
+			// printing top and bottom of visual structure
+			static void printTopNBot(string testarray[], int arraysize){
+				int length = arraysize / sizeof(string);
+				int numlength[length] = {};
+				countDigits(testarray,numlength,length);
+				cout<<"+";
+				for(int i = 0; i < length; i++){
+					for(int y = 0; y < numlength[i]; y++){
+						cout<<"-";
+					}
+					cout<<"+";
+				}
+				cout<<endl;
+			}
+			// printing numbers with dividers between
+			static void printNums(string testarray[], int arraysize){
+				arraysize /= sizeof(string);
+				cout<<"|";
+				for(int i = 0; i < arraysize; i++){
+					cout<<testarray[i]<<"|";
+				}
+				cout<<endl;
+			}
+			// countig number of digits in each number
+			static void countDigits(bool testarray[], int numlength[], int length){
+				for(int i = 0; i < length; i++){
+					string s = to_string(testarray[i]);
+					numlength[i] = s.length();
+				}
+			}
+			// printing top and bottom of visual structure
+			static void printTopNBot(bool testarray[], int arraysize){
+				int length = arraysize / sizeof(bool);
+				int numlength[length] = {};
+				countDigits(testarray,numlength,length);
+				cout<<"+";
+				for(int i = 0; i < length; i++){
+					for(int y = 0; y < numlength[i]; y++){
+						cout<<"-";
+					}
+					cout<<"+";
+				}
+				cout<<endl;
+			}
+			// printing numbers with dividers between
+			static void printNums(bool testarray[], int arraysize){
+				arraysize /= sizeof(bool);
+				cout<<"|";
+				for(int i = 0; i < arraysize; i++){
+					cout<<testarray[i]<<"|";
+				}
+				cout<<endl;
+			}			
             //Overloads for each data type
             //ints
             static void print(int testarray[], int arraysize)
-            {
-                for (int i = 0; i < arraysize / sizeof(int); i++){
-                    cout<<testarray[i];
-                    cout<<" ";
-                }
+            {	
+				printTopNBot(testarray,arraysize);
+				printNums(testarray,arraysize);
+				printTopNBot(testarray,arraysize);				
             }
             //long long int error bait
             static void print(long testarray[], int arraysize)
             {
-                for (int i = 0; i < arraysize / sizeof(long); i++){
-                    cout<<testarray[i];
-                    cout<<" ";
-                }
+				printTopNBot(testarray,arraysize);
+				printNums(testarray,arraysize);
+				printTopNBot(testarray,arraysize);		
             }
             //floats
             static void print(float testarray[], int arraysize)
             {
-                for (int i = 0; i < arraysize / sizeof(float); i++){
-                    cout<<testarray[i];
-                    cout<<" ";
-                }
+				printTopNBot(testarray,arraysize);
+				printNums(testarray,arraysize);
+				printTopNBot(testarray,arraysize);		
             }
             //double
             static void print(double testarray[], int arraysize)
             {
-                for (int i = 0; i < arraysize / sizeof(double); i++){
-                    cout<<testarray[i];
-                    cout<<" ";
-                }
+				printTopNBot(testarray,arraysize);
+				printNums(testarray,arraysize);
+				printTopNBot(testarray,arraysize);		
             }
             //char
             static void print(char testarray[], int arraysize)
             {
-                for (int i = 0; i < arraysize / sizeof(char); i++){
-                    cout<<testarray[i];
-                    cout<<" ";
-                }
+				printTopNBot(testarray,arraysize);
+				printNums(testarray,arraysize);
+				printTopNBot(testarray,arraysize);		
             }
             //string
             static void print(string testarray[], int arraysize)
             {
-                for (int i = 0; i < arraysize / sizeof(string); i++){
-                    cout<<testarray[i];
-                    cout<<" ";
-                }
+				printTopNBot(testarray,arraysize);
+				printNums(testarray,arraysize);
+				printTopNBot(testarray,arraysize);		
             }
             //bool
             static void print(bool testarray[], int arraysize)
             {
-                for (int i = 0; i < arraysize / sizeof(bool); i++){
-                    cout<<testarray[i];
-                    cout<<" ";
-                }
+				printTopNBot(testarray,arraysize);
+				printNums(testarray,arraysize);
+				printTopNBot(testarray,arraysize);		
             }
 
             //Going to try and add the same thing for stacks and queues soon
@@ -95,12 +296,13 @@ namespace printer
                             cout << "Press any key to advance...\n";
                             anykey();
                             cout << "\n--Current State--\n";
-                            for(k=0; k<=len-1; k++){
+                            /*for(k=0; k<=len-1; k++){
                                 if(testarray[k] == testarray[i] )
                                     cout << testarray[k] << " ";
                                 else
                                     cout << testarray[k] << " ";
-                            }
+                            }*/
+							print(testarray, size);
                             cout << "\n";
                         }
                     }
