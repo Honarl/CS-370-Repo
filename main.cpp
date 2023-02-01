37e7521d6ce05c6f10aa6e02ae9642391dba0750
Honarl <rogersmq@sunypoly.edu>
2023-02-01 01:41:11 -0500

    Stack/Queue
    I am in pain.
    We cannot use namespace std now?
    But we can in the header file and that's what's important.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,28 @@
 #include "print.h"
 #include <iostream>
+#include <stack>
+#include <queue>
 
-using namespace printer;
+
 
 int main(void)
 {
+
+    queue<int> testQueue;
+    testQueue.push(15);
+    testQueue.push(5);
+    testQueue.push(32);
+    printer::printerclass::print(testQueue);
+
+    stack<string> testStack;
+    testStack.push("CS 370");
+    testStack.push("All your data types are cause me pain");
+    testStack.push("Vertical storage BAYBEEE");
+    printer::printerclass::print(testStack);
+
     int testarray[5] {15, 5, 32, 500000000, 1000};
 
-    printerclass::bubbleSort(testarray, sizeof(testarray));
+    printer::printerclass::bubbleSort(testarray, sizeof(testarray));
 
     return 0;
 }
