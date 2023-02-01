8d469b1e6a804023ecdb1e8f4fb33b9a788f43f5
Honarl <rogersmq@sunypoly.edu>
2023-02-01 10:13:17 -0500

    I am pain
    Don't worry about it

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
 #include "print.h"
 #include <iostream>
+#include <stack>
+#include <queue>
 
 using namespace printer;
 
 int main(void)
 {
+
+    stack<int> testStack;
+    testStack.push(15);
+    testStack.push(35);
+    testStack.push(12);
+    printerclass::print(testStack);
+
+    queue<string> testQueue;
+    testQueue.push("TESTING");
+    testQueue.push("All your data types are give me pain");
+    testQueue.push("CS 370");
+    printerclass::print(testQueue);
+
     int testarray[5] {15, 5, 32, 500000000, 1000};
 //	long testlong[5] {586941.658941,6985.416958241,698541.695841,65841.5841,65841.65841};
 	float testfloat[5] {5.2,64.875,349.22222,0.27943,1000000.3168};
--- print.h
+++ print.h
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <string>
 #include "rogueutil.h"
+#include <stack>
+#include <queue>
 
 using namespace std;
 using namespace rogueutil;
@@ -131,7 +133,7 @@ namespace printer
 					cout<<testarray[i]<<"|";
 				}
 				cout<<endl;
-			}			
+			}
 			// countig number of chars
 /*			static void countDigits(char testarray[], int numlength[], int length){
 				for(int i = 0; i < length; i++){
@@ -164,7 +166,7 @@ namespace printer
 				for(int i =0; i < length; i++){
 					numlength[i] = testarray[i].length();
 				}
-		
+
 			}
 			// printing top and bottom of visual structure
 			static void printTopNBot(string testarray[], int arraysize){
@@ -218,60 +220,192 @@ namespace printer
 					cout<<testarray[i]<<"|";
 				}
 				cout<<endl;
-			}			
+			}
             //Overloads for each data type
             //ints
             static void print(int testarray[], int arraysize)
-            {	
+            {
 				printTopNBot(testarray,arraysize);
 				printNums(testarray,arraysize);
-				printTopNBot(testarray,arraysize);				
+				printTopNBot(testarray,arraysize);
             }
             //long long int error bait
             static void print(long testarray[], int arraysize)
             {
 				printTopNBot(testarray,arraysize);
 				printNums(testarray,arraysize);
-				printTopNBot(testarray,arraysize);		
+				printTopNBot(testarray,arraysize);
             }
             //floats
             static void print(float testarray[], int arraysize)
             {
 				printTopNBot(testarray,arraysize);
 				printNums(testarray,arraysize);
-				printTopNBot(testarray,arraysize);		
+				printTopNBot(testarray,arraysize);
             }
             //double
             static void print(double testarray[], int arraysize)
             {
 				printTopNBot(testarray,arraysize);
 				printNums(testarray,arraysize);
-				printTopNBot(testarray,arraysize);		
+				printTopNBot(testarray,arraysize);
             }
             //char
             static void print(char testarray[], int arraysize)
             {
 				printTopNBot(testarray,arraysize);
 				printNums(testarray,arraysize);
-				printTopNBot(testarray,arraysize);		
+				printTopNBot(testarray,arraysize);
             }
             //string
             static void print(string testarray[], int arraysize)
             {
 				printTopNBot(testarray,arraysize);
 				printNums(testarray,arraysize);
-				printTopNBot(testarray,arraysize);		
+				printTopNBot(testarray,arraysize);
             }
             //bool
             static void print(bool testarray[], int arraysize)
             {
 				printTopNBot(testarray,arraysize);
 				printNums(testarray,arraysize);
-				printTopNBot(testarray,arraysize);		
+				printTopNBot(testarray,arraysize);
             }
 
             //Going to try and add the same thing for stacks and queues soon
-
+            static void print(stack<int>& teststack){
+                stack<int> tempStack;
+                tempStack = teststack;
+                while (!tempStack.empty()){
+                    cout<<tempStack.top();
+                    tempStack.pop();
+                    cout<<endl;
+                }
+            }
+            static void print(stack<long>& teststack){
+                stack<long> tempStack;
+                tempStack = teststack;
+                while (!tempStack.empty()){
+                    cout<<tempStack.top();
+                    tempStack.pop();
+                    cout<<endl;
+                }
+            }
+            static void print(stack<float>& teststack){
+                stack<float> tempStack;
+                tempStack = teststack;
+                while (!tempStack.empty()){
+                    cout<<tempStack.top();
+                    tempStack.pop();
+                    cout<<endl;
+                }
+            }
+            static void print(stack<double>& teststack){
+                stack<double> tempStack;
+                tempStack = teststack;
+                while (!tempStack.empty()){
+                    cout<<tempStack.top();
+                    tempStack.pop();
+                    cout<<endl;
+                }
+            }
+            static void print(stack<char>& teststack){
+                stack<char> tempStack;
+                tempStack = teststack;
+                while (!tempStack.empty()){
+                    cout<<tempStack.top();
+                    tempStack.pop();
+                    cout<<endl;
+                }
+            }
+            static void print(stack<string>& teststack){
+                stack<string> tempStack;
+                tempStack = teststack;
+                while (!tempStack.empty()){
+                    cout<<tempStack.top();
+                    tempStack.pop();
+                    cout<<endl;
+                }
+            }
+            static void print(stack<bool>& teststack){
+                stack<bool> tempStack;
+                tempStack = teststack;
+                while (!tempStack.empty()){
+                    cout<<tempStack.top();
+                    tempStack.pop();
+                    cout<<endl;
+                }
+            }
+            static void print(queue<int>& testQueue){
+                queue<int> tempQueue;
+                tempQueue = testQueue;
+                cout<<"FIRST IN QUEUE -> ";
+                while (!tempQueue.empty()){
+                    cout<<tempQueue.front()<<" ";
+                    tempQueue.pop();
+                }
+                cout<<"<- LAST IN QUEUE"<<endl;
+            }
+            static void print(queue<long>& testQueue){
+                queue<long> tempQueue;
+                tempQueue = testQueue;
+                cout<<"FIRST IN QUEUE -> ";
+                while (!tempQueue.empty()){
+                    cout<<tempQueue.front()<<" ";
+                    tempQueue.pop();
+                }
+                cout<<"<- LAST IN QUEUE"<<endl;
+            }
+            static void print(queue<float>& testQueue){
+                queue<float> tempQueue;
+                tempQueue = testQueue;
+                cout<<"FIRST IN QUEUE -> ";
+                while (!tempQueue.empty()){
+                    cout<<tempQueue.front()<<" ";
+                    tempQueue.pop();
+                }
+                cout<<"<- LAST IN QUEUE"<<endl;
+            }
+            static void print(queue<double>& testQueue){
+                queue<double> tempQueue;
+                tempQueue = testQueue;
+                cout<<"FIRST IN QUEUE -> ";
+                while (!tempQueue.empty()){
+                    cout<<tempQueue.front()<<" ";
+                    tempQueue.pop();
+                }
+                cout<<"<- LAST IN QUEUE"<<endl;
+            }
+            static void print(queue<char>& testQueue){
+                queue<char> tempQueue;
+                tempQueue = testQueue;
+                cout<<"FIRST IN QUEUE -> ";
+                while (!tempQueue.empty()){
+                    cout<<tempQueue.front()<<" ";
+                    tempQueue.pop();
+                }
+                cout<<"<- LAST IN QUEUE"<<endl;
+            }
+            static void print(queue<string>& testQueue){
+                queue<string> tempQueue;
+                tempQueue = testQueue;
+                cout<<"FIRST IN QUEUE -> ";
+                while (!tempQueue.empty()){
+                    cout<<tempQueue.front()<<" ";
+                    tempQueue.pop();
+                }
+                cout<<"<- LAST IN QUEUE"<<endl;
+            }
+            static void print(queue<bool>& testQueue){
+                queue<bool> tempQueue;
+                tempQueue = testQueue;
+                cout<<"FIRST IN QUEUE -> ";
+                while (!tempQueue.empty()){
+                    cout<<tempQueue.front()<<" ";
+                    tempQueue.pop();
+                }
+                cout<<"<- LAST IN QUEUE"<<endl;
+            }
             //End the stacks/queues section
 
             //bubble sort
