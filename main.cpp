d926ce4f3f100ae6424a2922aec8d9efee9f1dba
BetCKamas <kamase@sunypoly.edu>
2023-02-02 21:20:40 -0500

    templates
    code file is now under 100 lines

--- main.cpp
+++ main.cpp
@@ -7,30 +7,31 @@ using namespace printer;
 
 int main(void)
 {
+	void bubbleSort(int testarray[], int size);
 
     stack<int> testStack;
     testStack.push(15);
     testStack.push(35);
     testStack.push(12);
-    printerclass::print(testStack);
+    printerclass::printStack(testStack);
 
     queue<string> testQueue;
     testQueue.push("TESTING");
     testQueue.push("All 21 Data Types Are Give Me Pain");
     testQueue.push("I'm not lying there are 21 overloads of one function");
     testQueue.push("CS 370");
-    printerclass::print(testQueue);
+    printerclass::printQueue(testQueue);
 
     int testarray[5] {15, 5, 32, 500000000, 1000};
-//	long testlong[5] {586941.658941,6985.416958241,698541.695841,65841.5841,65841.65841};
+	long testlong[5] {58694189,698541695,69856958,658415841,6584165};
 	float testfloat[5] {5.2,64.875,349.22222,0.27943,1000000.3168};
 	double testdouble[5] {5489.357,25,2654.215,658.2,3597.486357};
 	char testchar[5] {'m','f','d','t','y'};
 	string teststring[5] {"test", "work", "for", "me", "please"};
 	bool testbool[5] {0,0,1,1,0};
 
-    printerclass::bubbleSort(testarray, sizeof(testarray));
-//	printerclass::print(testlong, sizeof(testlong));
+	bubbleSort(testarray, sizeof(testarray));
+	printerclass::print(testlong, sizeof(testlong));
 	printerclass::print(testfloat, sizeof(testfloat));
 	printerclass::print(testdouble, sizeof(testdouble));
 	printerclass::print(testchar, sizeof(testchar));
@@ -39,3 +40,39 @@ int main(void)
 
     return 0;
 }
+
+//bubble sort
+    void bubbleSort(int testarray[], int size)
+	{
+		int i,j,k,curr;
+		int len = size / sizeof(int);
+		//Greet user
+		string user = getUsername();
+		cout << "Hi "<< user << ", heres your bubble sort\n";
+
+		//Sort array comparing index value with next Pos
+		for(i=0;i<=len-2;i++){
+			for(j=i+1;j<=len-1;j++){
+				if(testarray[i] > testarray[j]){
+					//Change value with next Position
+					curr = testarray[i];
+					testarray[i] = testarray[j];
+					testarray[j] = curr;
+
+					//Print current state of array
+					cout << "Press any key to advance...\n";
+					anykey();
+					cout << "\n--Current State--\n";
+					/*for(k=0; k<=len-1; k++){
+						if(testarray[k] == testarray[i] )
+							cout << testarray[k] << " ";
+						else
+							cout << testarray[k] << " ";
+					}*/
+					    printerclass::print(testarray, size);
+					cout << "\n";
+				}
+			}
+		}
+		//TODO - Underline values being switched
+	}
--- print.h
+++ print.h
@@ -1,10 +1,11 @@
 #ifndef PRINT_H_INCLUDED
 #define PRINT_H_INCLUDED
 #include <iostream>
-#include <string>
+#include <cmath>
 #include "rogueutil.h"
 #include <stack>
 #include <queue>
+#include <typeinfo>
 
 using namespace std;
 using namespace rogueutil;
@@ -14,258 +15,38 @@ namespace printer
     class printerclass
     {
         public:
-			// countig number of digits in each number
-			static void countDigits(int testarray[], int numlength[], int length){
-				for(int i = 0; i < length; i++){
-					string s = to_string(testarray[i]);
-					numlength[i] = s.length();
-				}
-			}
-			// printing top and bottom of visual structure
-			static void printTopNBot(int testarray[], int arraysize){
-				int length = arraysize / sizeof(int);
-				int numlength[length] = {};
-				countDigits(testarray,numlength,length);
+			template<typename T>
+			static void printTopNBot(T testarray[], int arraysize){
+				int length = arraysize / sizeof(T);
+				//int numlength[length] = {};
 				cout<<"+";
 				for(int i = 0; i < length; i++){
-					for(int y = 0; y < numlength[i]; y++){
+					for(int y = 0; y < 10; y++){
 						cout<<"-";
 					}
 					cout<<"+";
 				}
 				cout<<endl;
 			}
-			// printing numbers with dividers between
-			static void printNums(int testarray[], int arraysize){
-				arraysize /= sizeof(int);
+			template<typename T>
+			static void printNums(T testarray[], int arraysize){
+				arraysize /= sizeof(T);
 				cout<<"|";
 				for(int i = 0; i < arraysize; i++){
 					cout<<testarray[i]<<"|";
 				}
 				cout<<endl;
 			}
-			// countig number of digits in each number
-			static void countDigits(long testarray[], int numlength[], int length){
+			// counting number of digits in each number
+/*			static void countDigits(int testarray[], int numlength[], int length){
 				for(int i = 0; i < length; i++){
 					string s = to_string(testarray[i]);
 					numlength[i] = s.length();
 				}
 			}
-			// printing top and bottom of visual structure
-			static void printTopNBot(long testarray[], int arraysize){
-				int length = arraysize / sizeof(long);
-				int numlength[length] = {};
-				countDigits(testarray,numlength,length);
-				cout<<"+";
-				for(int i = 0; i < length; i++){
-					for(int y = 0; y < numlength[i]; y++){
-						cout<<"-";
-					}
-					cout<<"+";
-				}
-				cout<<endl;
-			}
-			// printing numbers with dividers between
-			static void printNums(long testarray[], int arraysize){
-				arraysize /= sizeof(long);
-				cout<<"|";
-				for(int i = 0; i < arraysize; i++){
-					cout<<testarray[i]<<"|";
-				}
-				cout<<endl;
-			}
-			// countig number of digits in each number
-			static void countDigits(float testarray[], int numlength[], int length){
-				for(int i = 0; i < length; i++){
-					string s = to_string(testarray[i]);
-					numlength[i] = s.length();
-				}
-			}
-			// printing top and bottom of visual structure
-			static void printTopNBot(float testarray[], int arraysize){
-				int length = arraysize / sizeof(float);
-				int numlength[length] = {};
-				countDigits(testarray,numlength,length);
-				cout<<"+";
-				for(int i = 0; i < length; i++){
-					for(int y = 0; y < numlength[i]; y++){
-						cout<<"-";
-					}
-					cout<<"+";
-				}
-				cout<<endl;
-			}
-			// printing numbers with dividers between
-			static void printNums(float testarray[], int arraysize){
-				arraysize /= sizeof(float);
-				cout<<"|";
-				for(int i = 0; i < arraysize; i++){
-					cout<<testarray[i]<<"|";
-				}
-				cout<<endl;
-			}
-			// countig number of digits in each number
-			static void countDigits(double testarray[], int numlength[], int length){
-				for(int i = 0; i < length; i++){
-					string s = to_string(testarray[i]);
-					numlength[i] = s.length();
-				}
-			}
-			// printing top and bottom of visual structure
-			static void printTopNBot(double testarray[], int arraysize){
-				int length = arraysize / sizeof(double);
-				int numlength[length] = {};
-				countDigits(testarray,numlength,length);
-				cout<<"+";
-				for(int i = 0; i < length; i++){
-					for(int y = 0; y < numlength[i]; y++){
-						cout<<"-";
-					}
-					cout<<"+";
-				}
-				cout<<endl;
-			}
-			// printing numbers with dividers between
-			static void printNums(double testarray[], int arraysize){
-				arraysize /= sizeof(double);
-				cout<<"|";
-				for(int i = 0; i < arraysize; i++){
-					cout<<testarray[i]<<"|";
-				}
-				cout<<endl;
-			}
-			// countig number of chars
-/*			static void countDigits(char testarray[], int numlength[], int length){
-				for(int i = 0; i < length; i++){
-					string s = to_string(testarray[i]);
-					numlength[i] = s.length();
-				}
-			}
-*/			// printing top and bottom of visual structure
-			static void printTopNBot(char testarray[], int arraysize){
-				int length = arraysize / sizeof(char);
-//				int numlength[length] = {};
-//				countDigits(testarray,numlength,length);
-				cout<<"+";
-				for(int i = 0; i < length; i++){
-					cout<<"-+";
-				}
-				cout<<endl;
-			}
-			// printing chars with dividers between
-			static void printNums(char testarray[], int arraysize){
-				arraysize /= sizeof(char);
-				cout<<"|";
-				for(int i = 0; i < arraysize; i++){
-					cout<<testarray[i]<<"|";
-				}
-				cout<<endl;
-			}
-			// countig number of digits in each number
-			static void countDigits(string testarray[], int numlength[], int length){
-				for(int i =0; i < length; i++){
-					numlength[i] = testarray[i].length();
-				}
-
-			}
-			// printing top and bottom of visual structure
-			static void printTopNBot(string testarray[], int arraysize){
-				int length = arraysize / sizeof(string);
-				int numlength[length] = {};
-				countDigits(testarray,numlength,length);
-				cout<<"+";
-				for(int i = 0; i < length; i++){
-					for(int y = 0; y < numlength[i]; y++){
-						cout<<"-";
-					}
-					cout<<"+";
-				}
-				cout<<endl;
-			}
-			// printing numbers with dividers between
-			static void printNums(string testarray[], int arraysize){
-				arraysize /= sizeof(string);
-				cout<<"|";
-				for(int i = 0; i < arraysize; i++){
-					cout<<testarray[i]<<"|";
-				}
-				cout<<endl;
-			}
-			// countig number of digits in each number
-			static void countDigits(bool testarray[], int numlength[], int length){
-				for(int i = 0; i < length; i++){
-					string s = to_string(testarray[i]);
-					numlength[i] = s.length();
-				}
-			}
-			// printing top and bottom of visual structure
-			static void printTopNBot(bool testarray[], int arraysize){
-				int length = arraysize / sizeof(bool);
-				int numlength[length] = {};
-				countDigits(testarray,numlength,length);
-				cout<<"+";
-				for(int i = 0; i < length; i++){
-					for(int y = 0; y < numlength[i]; y++){
-						cout<<"-";
-					}
-					cout<<"+";
-				}
-				cout<<endl;
-			}
-			// printing numbers with dividers between
-			static void printNums(bool testarray[], int arraysize){
-				arraysize /= sizeof(bool);
-				cout<<"|";
-				for(int i = 0; i < arraysize; i++){
-					cout<<testarray[i]<<"|";
-				}
-				cout<<endl;
-			}
-            //Overloads for each data type
-            //ints
-            static void print(int testarray[], int arraysize)
-            {
-				printTopNBot(testarray,arraysize);
-				printNums(testarray,arraysize);
-				printTopNBot(testarray,arraysize);
-            }
-            //long long int error bait
-            static void print(long testarray[], int arraysize)
-            {
-				printTopNBot(testarray,arraysize);
-				printNums(testarray,arraysize);
-				printTopNBot(testarray,arraysize);
-            }
-            //floats
-            static void print(float testarray[], int arraysize)
-            {
-				printTopNBot(testarray,arraysize);
-				printNums(testarray,arraysize);
-				printTopNBot(testarray,arraysize);
-            }
-            //double
-            static void print(double testarray[], int arraysize)
-            {
-				printTopNBot(testarray,arraysize);
-				printNums(testarray,arraysize);
-				printTopNBot(testarray,arraysize);
-            }
-            //char
-            static void print(char testarray[], int arraysize)
-            {
-				printTopNBot(testarray,arraysize);
-				printNums(testarray,arraysize);
-				printTopNBot(testarray,arraysize);
-            }
-            //string
-            static void print(string testarray[], int arraysize)
-            {
-				printTopNBot(testarray,arraysize);
-				printNums(testarray,arraysize);
-				printTopNBot(testarray,arraysize);
-            }
-            //bool
-            static void print(bool testarray[], int arraysize)
+*/	 
+			template<typename T>
+            static void print(T testarray[], int arraysize)
             {
 				printTopNBot(testarray,arraysize);
 				printNums(testarray,arraysize);
@@ -273,9 +54,10 @@ namespace printer
             }
 
             //Going to try and add the same thing for stacks and queues soon
-            static void print(stack<int>& teststack){
+			template<typename T>
+            static void printStack(stack<T>& teststack){
                 //Create a temporary stack to hold the info
-                stack<int> tempStack;
+                stack<T> tempStack;
                 //Clone the input stack
                 tempStack = teststack;
                 while (!tempStack.empty()){
@@ -287,93 +69,10 @@ namespace printer
                     cout<<endl;
                 }
             }
-            static void print(stack<long>& teststack){
-                //Temporary info holding stack
-                stack<long> tempStack;
-                //Clone
-                tempStack = teststack;
-                while (!tempStack.empty()){
-                    //Spit out top item
-                    cout<<tempStack.top();
-                    //Get rid of it
-                    tempStack.pop();
-                    //Formatting
-                    cout<<endl;
-                }
-            }
-            static void print(stack<float>& teststack){
-                //Make a temporary shell
-                stack<float> tempStack;
-                //Clone the passed one
-                tempStack = teststack;
-                while (!tempStack.empty()){
-                    //Give the the top item
-                    cout<<tempStack.top();
-                    //Remove the top item
-                    tempStack.pop();
-                    //Look like a stack
-                    cout<<endl;
-                }
-            }
-            static void print(stack<double>& teststack){
-                //Temporary stack
-                stack<double> tempStack;
-                //Attack of the clones
-                tempStack = teststack;
-                while (!tempStack.empty()){
-                    //Spit the top out
-                    cout<<tempStack.top();
-                    //KILL
-                    tempStack.pop();
-                    //It's VERTICAL
-                    cout<<endl;
-                }
-            }
-            static void print(stack<char>& teststack){
-                //Letter Stack
-                stack<char> tempStack;
-                //Skinwalker Stack
-                tempStack = teststack;
-                while (!tempStack.empty()){
-                    //Tell me what the stack do
-                    cout<<tempStack.top();
-                    //Eliminate the top guy
-                    tempStack.pop();
-                    //Straight line
-                    cout<<endl;
-                }
-            }
-            static void print(stack<string>& teststack){
-                //New Temporary
-                stack<string> tempStack;
-                //Human cloning is unethical
-                tempStack = teststack;
-                while (!tempStack.empty()){
-                    //Find the first thing
-                    cout<<tempStack.top();
-                    //Kill John Lennon
-                    tempStack.pop();
-                    //John Lennon has his own line
-                    cout<<endl;
-                }
-            }
-            static void print(stack<bool>& teststack){
-                //Make more bools
-                stack<bool> tempStack;
-                //Copy dem bools
-                tempStack = teststack;
-                while (!tempStack.empty()){
-                    //Tell me why
-                    cout<<tempStack.top();
-                    //Damn that heartbreak killed him
-                    tempStack.pop();
-                    //Uhhh line
-                    cout<<endl;
-                }
-            }
-            static void print(queue<int>& testQueue){
+			template<typename T>
+            static void printQueue(queue<T>& testQueue){
                 //Make the temporary queue
-                queue<int> tempQueue;
+                queue<T> tempQueue;
                 //Clone the temporary queue
                 tempQueue = testQueue;
                 //Tell me who is first
@@ -387,103 +86,10 @@ namespace printer
                 //Tell me what's at the end and then endl for formatting
                 cout<<"<- LAST IN QUEUE"<<endl;
             }
-            static void print(queue<long>& testQueue){
-                queue<long> tempQueue;
-                tempQueue = testQueue;
-                cout<<"FIRST IN QUEUE -> ";
-                while (!tempQueue.empty()){
-                    cout<<tempQueue.front()<<" ";
-                    tempQueue.pop();
-                }
-                cout<<"<- LAST IN QUEUE"<<endl;
-            }
-            static void print(queue<float>& testQueue){
-                queue<float> tempQueue;
-                tempQueue = testQueue;
-                cout<<"FIRST IN QUEUE -> ";
-                while (!tempQueue.empty()){
-                    cout<<tempQueue.front()<<" ";
-                    tempQueue.pop();
-                }
-                cout<<"<- LAST IN QUEUE"<<endl;
-            }
-            static void print(queue<double>& testQueue){
-                queue<double> tempQueue;
-                tempQueue = testQueue;
-                cout<<"FIRST IN QUEUE -> ";
-                while (!tempQueue.empty()){
-                    cout<<tempQueue.front()<<" ";
-                    tempQueue.pop();
-                }
-                cout<<"<- LAST IN QUEUE"<<endl;
-            }
-            static void print(queue<char>& testQueue){
-                queue<char> tempQueue;
-                tempQueue = testQueue;
-                cout<<"FIRST IN QUEUE -> ";
-                while (!tempQueue.empty()){
-                    cout<<tempQueue.front()<<" ";
-                    tempQueue.pop();
-                }
-                cout<<"<- LAST IN QUEUE"<<endl;
-            }
-            static void print(queue<string>& testQueue){
-                queue<string> tempQueue;
-                tempQueue = testQueue;
-                cout<<"FIRST IN QUEUE -> ";
-                while (!tempQueue.empty()){
-                    cout<<tempQueue.front()<<" ";
-                    tempQueue.pop();
-                }
-                cout<<"<- LAST IN QUEUE"<<endl;
-            }
-            static void print(queue<bool>& testQueue){
-                queue<bool> tempQueue;
-                tempQueue = testQueue;
-                cout<<"FIRST IN QUEUE -> ";
-                while (!tempQueue.empty()){
-                    cout<<tempQueue.front()<<" ";
-                    tempQueue.pop();
-                }
-                cout<<"<- LAST IN QUEUE"<<endl;
-            }
+ 
             //End the stacks/queues section
 
-            //bubble sort
-            static void bubbleSort(int testarray[], int size)
-            {
-                int i,j,k,curr;
-                int len = size / sizeof(int);
-                //Greet user
-                string user = getUsername();
-                cout << "Hi "<< user << ", heres your bubble sort\n";
-
-                //Sort array comparing index value with next Pos
-                for(i=0;i<=len-2;i++){
-                    for(j=i+1;j<=len-1;j++){
-                        if(testarray[i] > testarray[j]){
-                            //Change value with next Position
-                            curr = testarray[i];
-                            testarray[i] = testarray[j];
-                            testarray[j] = curr;
-
-                            //Print current state of array
-                            cout << "Press any key to advance...\n";
-                            anykey();
-                            cout << "\n--Current State--\n";
-                            /*for(k=0; k<=len-1; k++){
-                                if(testarray[k] == testarray[i] )
-                                    cout << testarray[k] << " ";
-                                else
-                                    cout << testarray[k] << " ";
-                            }*/
-							print(testarray, size);
-                            cout << "\n";
-                        }
-                    }
-                }
-                //TODO - Underline values being switched
-            }
+            
     };
 }
 
