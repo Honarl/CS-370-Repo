166d75cee0615ec8c2270716c149de6fa38b5cb1
BetCKamas <kamase@sunypoly.edu>
2023-02-08 14:31:03 -0500

    demo code

--- main.cpp
+++ main.cpp
@@ -14,30 +14,28 @@ int main(void)
     testStack.push(35);
     testStack.push(12);
     printerclass::printStack(testStack);
+	cout<<endl;
 
+	anykey();
 
     queue<string> testQueue;
     testQueue.push("TESTING");
     testQueue.push("All 21 Data Types Are Give Me Pain");
-    testQueue.push("I'm not lying there are 21 overloads of one function");
+    testQueue.push("I'm not lying there were are 21 overloads of one function");
     testQueue.push("CS 370");
     printerclass::printQueue(testQueue);
+	cout<<endl;
+	
+	anykey();
 
-    int testarray[5] {15, 5, 32, 500000000, 1000};
-	long testlong[5] {58694189,698541695,69856958,658415841,6584165};
+    int testint[5] {15, 5, 32, 500000000, 1000};
 	float testfloat[5] {5.2,64.875,349.22222,0.27943,1000000.3168};
-	double testdouble[5] {5489.357,25,2654.215,658.2,3597.486357};
-	char testchar[5] {'m','f','d','t','y'};
-	string teststring[5] {"test", "work", "for", "me", "please"};
-	bool testbool[5] {0,0,1,1,0};
 
-	bubbleSort(testarray, sizeof(testarray));
-//	printerclass::print(testlong, sizeof(testlong));
-//	printerclass::print(testfloat, sizeof(testfloat));
-//	printerclass::print(testdouble, sizeof(testdouble));
-//	printerclass::print(testchar, sizeof(testchar));
-//	printerclass::print(teststring, sizeof(teststring));
-//	printerclass::print(testbool, sizeof(testbool));
+
+	printerclass::printArray(testfloat, sizeof(testfloat));
+	anykey();
+
+	bubbleSort(testint, sizeof(testint));
 
     return 0;
 }
@@ -47,10 +45,7 @@ int main(void)
 	{
 		int i,j,k,curr;
 		int len = size / sizeof(int);
-		//Greet user
-		string user = getUsername();
-		cout << "Hi "<< user << ", heres your bubble sort\n";
-		cout<<"--Starting state--"<<endl;
+		cout<<"--Starting bubble sort state--"<<endl;
 		printerclass::printArray(testarray, size);
 
 		//Sort array comparing index value with next Pos
